62e91f89fc2d3117b9f23fdffd441f1cc7f1c0cd
AndrExAgris <andredecampos.ac@gmail.com>
2023-11-02 21:18:56 -0300

    agr ele le a fita

--- pda.py
+++ pda.py
@@ -1,25 +1,25 @@
 
-# lendo a quintupla para um arquivo -------------------------------------------------
+# lendo a ntuplaupla para um arquivo -------------------------------------------------
 f = open("inputfile", "r")
 
-quint = []
+ntupla = []
 
 l = f.readline()
 estados = l.split("#")[0]
 estados = estados.split()
-quint.append(estados)
+ntupla.append(estados)
 l = f.readline()
 
 l = f.readline()
 alfab = l.split("#")[0]
 alfab = alfab.split()
-quint.append(alfab)
+ntupla.append(alfab)
 l = f.readline()
 
 l = f.readline()
 pilha = l.split("#")[0]
 pilha = pilha.split()
-quint.append(pilha)
+ntupla.append(pilha)
 l = f.readline()
 
 l = f.readline()
@@ -29,81 +29,83 @@ while (l != "\n"):
     t = [x if x!="''" else '§' for x in l.split()]
     trans.append(t)
 trans.pop()
-quint.append(trans)
+ntupla.append(trans)
 
 l = f.readline()
 init = l.split("#")[0]
 init = init.split()
-quint.append(init)
+ntupla.append(init)
 
 f.readline()
 l = f.readline()
 termin = l.split("#")[0]
 termin = termin.split()
-quint.append(termin)
+ntupla.append(termin)
 
 #criando o arquivo do graphviz -------------------------------------------------------
-gra = open("graphpy.gv", "w+")
+gra = open("automata.gv", "w+")
 
 gra.write(' digraph X {\n rankdir=LR;\n')
 
 gra.write(' init [shape=point]\n')
 
-for x in quint[4]:
+for x in ntupla[4]:
     gra.write(' init -> {}\n'.format(x))
 
 finais = " node[shape=doublecircle] "
-for x in quint[5]:
+for x in ntupla[5]:
     finais = finais + x
 gra.write(finais + ";\n")
 
 gra.write(' node[shape=circle]\n')
 
-for x in quint[3]:
+for x in ntupla[3]:
     gra.write(' {} -> {} [label="{},{},{}"]\n'.format(x[0],x[4], x[1], x[2], x[3]))
 gra.write("}\n")
 
 
 #recebendo a fita -------------------------------------------------------------------
-print("Digite a fita (ex: 'abaaa')")
+print("Digite a fita (ex1: 'abaaa' ex2: '')")
 fita = input()
 
 posfita = 0
 stack = [] 
-estado = quint[4][0]
+estado = ntupla[4][0]
 
 for i in fita:
-    if all(i != item for item in quint[1]):
+    if all(i != item for item in ntupla[1]):
         print(i, " não é aceito pelo autamato \nfita não reconhecida")
         exit()
     
 
-    for j in quint[3]:
-        if estado == j[0]:
-            if i == j[1]:
-                if len(stack) != 0:   
-                    if stack[-1]== j[3]:  
-                        stack.pop()
-                        if j[2] != "ε":
-                            stack.append(j[2])
-                        estado = j[4]
-                        posfita+=1
-                        break
-                    elif j[3] == "ε":
-                        if j[2] != "ε":
-                            stack.append(j[2])
-                        estado = j[4]
-                        posfita+=1
-                        break
-                else:
-                    if j[3] == "ε":  
-                        if j[2] != "?":
-                            stack.append(j[2])
-                        estado = j[4]
-                        posfita+=1
-                        break
-                print("Fita não reconhecida")
+    for j in ntupla[3]:
+        if estado == j[0] and i == j[1]:
+            if len(stack) != 0 and (stack[-1] == j[2] or j[2] == "ε"):   
+                if stack[-1] == j[2]:
+                    stack.pop()
+                if j[3] != "ε":
+                    stack.append(j[3])
+                estado = j[4]
+                posfita+=1
                 break
-    
-if (estado in quint[5]):
-    print("Fita reconhecida")
+            else:
+                if j[2] == "ε":  
+                    if j[3] != "ε":
+                        stack.append(j[3])
+                    estado = j[4]
+                    posfita+=1
+                    break
+            print("Fita não reconhecida")
+            exit()
+
+if len(stack) == 0:
+    if any(estado == item for item in ntupla[5]):
+        print("fita reconhecida")
+    else:
+        for j in ntupla[3]:
+            if estado == j[0] and j[1] == "?" and j[2] == "?" and j[3] == "ε":
+                if any(j[4] == item for item in ntupla[5]):
+                    print("fita reconhecida")
+                    break
+else:
+    print("fita não reconhecida")
